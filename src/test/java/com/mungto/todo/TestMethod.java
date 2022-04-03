c05dba25766489cd948d92af09ce00bdffa84e4c
mungto <daum7766@naver.com>
2022-04-03 20:53:30 +0900

    docs: restdocs 추가

--- src/test/java/com/mungto/todo/TestMethod.java
+++ src/test/java/com/mungto/todo/TestMethod.java
@@ -3,40 +3,51 @@ package com.mungto.todo;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
 
 public enum TestMethod {
     GET {
         @Override
-        public ExtractableResponse<Response> extractedResponse(RequestSpecification request, String url) {
-            return request.when().get(url)
+        public ExtractableResponse<Response> extractedResponse(RequestSpecification request,
+            String url) {
+            return request.when()
+                .get(url)
                 .then()
                 .extract();
         }
     },
     POST {
         @Override
-        public ExtractableResponse<Response> extractedResponse(RequestSpecification request, String url) {
-            return request.when().post(url)
+        public ExtractableResponse<Response> extractedResponse(RequestSpecification request,
+            String url) {
+            return request.when()
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .post(url)
                 .then()
                 .extract();
         }
     },
     PUT {
         @Override
-        public ExtractableResponse<Response> extractedResponse(RequestSpecification request, String url) {
-            return request.when().put(url)
+        public ExtractableResponse<Response> extractedResponse(RequestSpecification request,
+            String url) {
+            return request.when()
+                .put(url)
                 .then()
                 .extract();
         }
     },
     DELETE {
         @Override
-        public ExtractableResponse<Response> extractedResponse(RequestSpecification request, String url) {
-            return request.when().delete(url)
+        public ExtractableResponse<Response> extractedResponse(RequestSpecification request,
+            String url) {
+            return request.when()
+                .delete(url)
                 .then()
                 .extract();
         }
     };
 
-    public abstract ExtractableResponse<Response> extractedResponse(RequestSpecification request, String url);
+    public abstract ExtractableResponse<Response> extractedResponse(RequestSpecification request,
+        String url);
 }
